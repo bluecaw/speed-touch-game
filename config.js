c50ecf1b83786f3d3692910d10ff5982af348880
bluecaw <passioncat007@gmail.com>
2026-04-04 23:24:45 +0900

    nosecure_commit

---
+++ config.js
@@ -0,0 +1,8 @@
+    const firebaseConfig = {
+        apiKey: "AIzaSyDmERQEIQKssVy12KodGDjwfIDbWcwBOmo",
+        authDomain: "speedtouch25-ca738.firebaseapp.com",
+        projectId: "speedtouch25-ca738",
+        storageBucket: "speedtouch25-ca738.appspot.com",
+        messagingSenderId: "865990228190",
+        appId: "1:865990228190:web:96e1a76f2648507851a719"
+    };
